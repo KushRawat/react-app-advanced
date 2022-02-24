5b89e663ae5d49f1525f8fd9da55709d0986ad81
Kush Rawat <rawatkush0704@gmail.com>
2022-02-25 00:45:56 +0530

    custom context provider component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../context/auth-context';
 
 const emailReducer = (prevState, action) => {
   if (action.type === 'USER_INPUT') {
@@ -43,6 +44,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
+
+  const ctx = useContext(AuthContext);
 
   // useEffect(() => {
   //   console.log('EFFECT RUNNING');
@@ -93,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
